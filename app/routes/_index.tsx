a1f5b6da82b260fe28e78e72b39f6a953abf3d10
nulfrost <khadane.miller@gmail.com>
2025-02-13 14:20:18 -0500

    chore: first versions of lexicons

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,6 @@
 import type { Route } from "./+types/_index";
-import { prisma } from "#/app/db.server";
 
 export async function loader({ request }: Route.LoaderArgs) {
-	await prisma.log.create({
-		data: {
-			uri: "at://some-url",
-			status: "PLANNED",
-			started_at: new Date(),
-			userId: "1",
-			gameId: "1234",
-		},
-	});
 	return {};
 }
 
